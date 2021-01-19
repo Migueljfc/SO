40efd9d6783128698123840434910789f97b5ad4
Miguel <miguel.f.cabral@ua.pt>
2021-01-19 23:01:59 +0000

    guiao3

---
+++ guiao2/03-client_server/client_server.cpp
@@ -0,0 +1,18 @@
+#define POOL_SIZE 100
+typedef int buf{
+    buffer[100];
+}
+
+
+
+
+
+
+
+class Service{
+private: 
+    ServiceStruct pool[POOL_SIZE]
+    bool hasResponse[POOL_SIZE];
+    pthread_cond
+
+}
